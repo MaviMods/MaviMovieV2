9eeba256da44e870c059db6684235b431c163ad7
Mavi Mods <139610566+MaviMods@users.noreply.github.com>
2024-05-25 07:34:40 +0000

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1625,8 +1625,8 @@ async def auto_filter(client, msg, spoll=False):
             user_id = message.from_user.id
             files, offset, total_results = await get_search_results(search.lower(), offset=0, filter=True)
             if not files:
-		    if settings["spell_check"]:
-                    return await advantage_spell_chok(msg)
+		if settings["spell_check"]:
+                return await advantage_spell_chok(msg)
 	    else:
                 await client.send_message(req_channel,f"-🦋 #REQUESTED_CONTENT 🦋-\n\n📝**Content Name** :`{search}`\n**Requested By**: {message.from_user.first_name}\n **USER ID**:{user_id}\n\n🗃️",
                                                                                                        reply_markup=InlineKeyboardMarkup([
