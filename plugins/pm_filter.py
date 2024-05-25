2ef5310f211ba5608f3b25947227033c74545ace
Mavi Mods <139610566+MaviMods@users.noreply.github.com>
2024-05-25 08:33:25 +0000

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1895,7 +1895,9 @@ async def auto_filter(client, msg, spoll=False):
         await msg.message.delete()
 
 # Born to make history @LazyDeveloper !
-async def advantage_spell_chok(msg):
+async def advantage_spell_chok(client, msg):
+    mv_id = msg.id
+    mv_rqst = msg.text
     query = re.sub(
         r"\b(pl(i|e)*?(s|z+|ease|se|ese|(e+)s(e)?)|((send|snd|giv(e)?|gib)(\sme)?)|movie(s)?|new|latest|br((o|u)h?)*|^h(e|a)?(l)*(o)*|mal(ayalam)?|t(h)?amil|file|that|find|und(o)*|kit(t(i|y)?)?o(w)?|thar(u)?(o)*w?|kittum(o)*|aya(k)*(um(o)*)?|full\smovie|any(one)|with\ssubtitle(s)?)",
         "", msg.text, flags=re.IGNORECASE)  # plis contribute some common words
